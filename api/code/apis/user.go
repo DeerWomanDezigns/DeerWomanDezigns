99fe39fbdc5cebd709170dc3228724ace45b0f90
Maybees <8634709+Maybees@users.noreply.github.com>
2021-10-10 14:44:24 -0700

    feat: Quick commit to sotre progress before repo move

---
+++ api/code/apis/user.go
@@ -0,0 +1,28 @@
+package apis
+
+import (
+	"github.com/Maybeenaught/deer-woman-dezigns/code/daos"
+	"github.com/Maybeenaught/deer-woman-dezigns/code/services"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// GetUser godoc
+// @Summary Retrieves user based on given ID
+// @Produce json
+// @Param id path integer true "User ID"
+// @Success 200 {object} models.User
+// @Router /users/{id} [get]
+// @Security ApiKeyAuth
+func GetUser(c *gin.Context) {
+	s := services.NewUserService(daos.NewUserDAO())
+	id, _ := strconv.ParseUint(c.Param("id"), 10, 32)
+	if user, err := s.Get(uint(id)); err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		log.Println(err)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
--- api/main.go
+++ api/main.go
@@ -1,51 +1,90 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
-	"github.com/go-yaml/yaml"
+	"github.com/Maybeenaught/deer-woman-dezigns/code/apis"
+	"github.com/Maybeenaught/deer-woman-dezigns/code/config"
+	"github.com/Maybeenaught/deer-woman-dezigns/code/httputil"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/Maybeenaught/deer-woman-dezigns/code/docs"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+// @title Deer Woman Dezigns Swagger API
+// @version 1.0
+// @description Swagger API for Deer Woman Dezigns.
+// @termsOfService http://swagger.io/terms/
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
+// @contact.name API Support
+// @contact.email deerwomandezigns.site@gmail.com
 
-func orderPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "orderPage!")
-	fmt.Println("Endpoint Hit: orderPage")
-}
+// @license.name MIT
+// @license.url https://github.com/Maybeenaught/DeerWomanDezigns/blob/main/license.md
 
-func handleRequests(port int) {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/order", orderPage)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
-}
+// @BasePath /api/v1
 
-type Config struct {
-	Port int `yaml:"port"`
-}
-
-func readConfig() Config {
-	dat, readErr := ioutil.ReadFile("webConfig.yaml")
-	check(readErr)
-	var config Config
-	yamlErr := yaml.Unmarshal(dat, &config)
-	check(yamlErr)
-	return config
-}
-
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	config := readConfig()
+	// load application configurations
+	if err := config.LoadConfig("config"); err != nil {
+		panic(fmt.Errorf("invalid application configuration: %s", err))
+	}
 
-	handleRequests(config.Port)
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	v1 := r.Group("/api/v1")
+	{
+		v1.Use(auth())
+		v1.GET("/users/:id", apis.GetUser)
+	}
+
+	config.Config.DB, config.Config.DBErr = gorm.Open("postgres", config.Config.DSN)
+	if config.Config.DBErr != nil {
+		panic(config.Config.DBErr)
+	}
+
+	// config.Config.DB.AutoMigrate(&models.User{}) // This is needed for generation of schema for postgres image.
+
+	defer config.Config.DB.Close()
+
+	log.Println("Successfully connected to database")
+
+	r.Run(fmt.Sprintf(":%v", config.Config.ServerPort))
+}
+
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if len(authHeader) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("authorization is a required header"))
+			c.Abort()
+		}
+		if authHeader != config.Config.ApiKey {
+			httputil.NewError(c, http.StatusUnauthorized, fmt.Errorf("this user isn't authorized to this operation: api_key=%s", authHeader))
+			c.Abort()
+		}
+		c.Next()
+	}
 }
