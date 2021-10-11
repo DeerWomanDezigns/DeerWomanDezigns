4714ae8330b95bd4bbc8e524188ac965f456deea
Maybees <8634709+Maybees@users.noreply.github.com>
2021-10-10 22:49:42 -0700

    fix: Switching form to json in model dec

--- api/code/models/model.go
+++ api/code/models/model.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	ID        string `dynamo:"user_id" form:"id" xml:"id" binding:"required"`
-	FirstName string `dynamo:"first_name" form:"firstName" binding:"required"`
-	LastName  string `dynamo:"last_name" form:"lastName" binding:"required"`
-	Address   string `dynamo:"address" form:"address" binding:"required"`
-	Email     string `dynamo:"email" form:"email" binding:"required"`
+	ID        string `dynamo:"user_id" json:"id" binding:"required"`
+	FirstName string `dynamo:"first_name" json:"firstName" binding:"required"`
+	LastName  string `dynamo:"last_name" json:"lastName" binding:"required"`
+	Address   string `dynamo:"address" json:"address" binding:"required"`
+	Email     string `dynamo:"email" json:"email" binding:"required"`
 }
