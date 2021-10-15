4ae3b7b2e3967d9a50da70c906acec2a6bad17c5
Maybees <8634709+Maybees@users.noreply.github.com>
2021-10-15 11:32:25 -0700

    refactor: Moving auth middleware to middleware package

---
+++ api/code/middleware/auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/config"
+	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/httputil"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if len(authHeader) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("authorization is a required header"))
+			c.Abort()
+		}
+		if authHeader != config.Config.ApiKey {
+			httputil.NewError(c, http.StatusUnauthorized, fmt.Errorf("this user isn't authorized to this operation: api_key=%s", authHeader))
+			c.Abort()
+		}
+		c.Next()
+	}
+}
--- api/main.go
+++ api/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/apis"
 	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/config"
-	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/httputil"
 	"github.com/deer-woman-dezigns/deer-woman-dezigns/code/middleware"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -54,13 +51,12 @@ func main() {
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	r.Use(gin.Recovery())
 
-	r.Use(middleware.CORS())
-
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	v1 := r.Group("/api/v1")
 	{
-		v1.Use(auth())
+		v1.Use(middleware.CORS())
+		v1.Use(middleware.Auth())
 		v1.GET("/users", apis.GetAllUsers)
 		v1.GET("/users/:id", apis.GetUser)
 		v1.POST("/users", apis.AddUser)
@@ -77,20 +73,5 @@ func main() {
 		autotls.Run(r, "deerwoman-dezigns.com")
 	} else {
 		r.Run(fmt.Sprintf(":%v", config.Config.ServerPort))
-	}
-}
-
-func auth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if len(authHeader) == 0 {
-			httputil.NewError(c, http.StatusUnauthorized, errors.New("authorization is a required header"))
-			c.Abort()
-		}
-		if authHeader != config.Config.ApiKey {
-			httputil.NewError(c, http.StatusUnauthorized, fmt.Errorf("this user isn't authorized to this operation: api_key=%s", authHeader))
-			c.Abort()
-		}
-		c.Next()
 	}
 }
