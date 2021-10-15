5e2646d98fe3645d55f269c0c154a246246a1fe9
Blake14 <blakemcpherson2@gmail.com>
2021-10-15 14:04:35 -0500

    feat: added logo to app

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logo.png';
+import './index.css';
+import Image from 'react-bootstrap/Image';
+import React from 'react';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
-      <Navigation />
+      <div className="body">
+        <Navigation />
+        <Image src={logo} className="logoMain"/>
+      </div>
     </div>
   );
 }
