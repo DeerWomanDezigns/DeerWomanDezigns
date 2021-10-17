b6b69ed2316e587fee30cebdf4d282f00015c4e1
Maybees <8634709+Maybees@users.noreply.github.com>
2021-10-17 00:45:48 -0700

    fix: Adding networking bits to lightsail tf

--- infra/lightsail.tf
+++ infra/lightsail.tf
@@ -16,6 +16,44 @@ resource "aws_lightsail_instance" "lightsail_instance_frontend" {
   user_data         = "${file("${path.module}/scripts/instance_setup.sh")}"
 }
 
+resource "aws_lightsail_static_ip" "lightsail_static_ip_backend" {
+  name = "backend_static"
+}
+
+resource "aws_lightsail_static_ip_attachment" "lightsail_static_ip_attachment_backend" {
+  static_ip_name = aws_lightsail_static_ip.lightsail_static_ip_backend.id
+  instance_name  = aws_lightsail_instance.lightsail_instance_backend.id
+}
+
+resource "aws_lightsail_static_ip" "lightsail_static_ip_frontend" {
+  name = "frontend_static"
+}
+
+resource "aws_lightsail_static_ip_attachment" "lightsail_static_ip_attachment_frontend" {
+  static_ip_name = aws_lightsail_static_ip.lightsail_static_ip_frontend.id
+  instance_name  = aws_lightsail_instance.lightsail_instance_frontend.id
+}
+
+resource "aws_lightsail_instance_public_ports" "lightsail_instance_public_ports_backend" {
+  instance_name = aws_lightsail_instance.lightsail_instance_backend.name
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+  }
+}
+
+resource "aws_lightsail_instance_public_ports" "lightsail_instance_public_ports_frontend" {
+  instance_name = aws_lightsail_instance.lightsail_instance_frontend.name
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+  }
+}
+
 resource "aws_lightsail_key_pair" "lightsail_key_pair_backend" {
   name       = "backend_ssh"
   public_key = file("${path.module}/keys/backend.pub")
