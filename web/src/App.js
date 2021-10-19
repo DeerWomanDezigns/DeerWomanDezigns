dcb249f5c72b7211dd92c525de241a30bdedc92c
Blake14 <blakemcpherson2@gmail.com>
2021-10-18 20:45:12 -0500

    style: close on background click

--- web/src/App.js
+++ web/src/App.js
@@ -2,23 +2,61 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './assets/logo_full.png';
 import './index.css';
 import Image from 'react-bootstrap/Image';
-import React from 'react';
-import Navigation from './components/Navigation';
 import UsersPage from './components/UsersPage';
 import Home from './components/Home';
 import Backdrop from './assets/Back.jpg';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { RiCloseFill, RiMore2Fill } from 'react-icons/ri';
+import { IconContext } from 'react-icons';
+import { SidebarData } from './components/sidebarData';
+import './components/navbar.css'
+
 
 var sectionStyle = {
   backgroundImage: `url(${Backdrop})`
 }
 
+
+
 function App() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar)
+
   return (
     <div style={sectionStyle}>
-      <div className="body">
+      <div className="body"  onClick={showSidebar}>
         <Router>
-          <Navigation />
+          <IconContext.Provider value={{ color: "white" }}>
+          <div>
+            <div className="navbar">
+              <Link to='#' className='menu-bars'>
+                <RiMore2Fill onClick={showSidebar} />
+              </Link>
+            </div>
+            <Nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+              <ul className='nav-menu-items'>
+                <li className='navbar-toggle'>
+                  <Link to="/" className='menu-bars'>
+                    <RiCloseFill onClick={showSidebar} />
+                  </Link>
+                </li>
+                {SidebarData.map((item, index) => {
+                  return (
+                    <li key={index} className={item.cName}>
+                      <Link to={item.path}>
+                        {item.icon}
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Nav>
+          </div>
+        </IconContext.Provider>
           <Image src={logo} className="logoMain" />
           <Switch>
             <Route exact path='/' component={Home} />
@@ -26,6 +64,8 @@ function App() {
             <Route path='/UsersPage' component={UsersPage} />
           </Switch>
         </Router>
+        
+
       </div>
     </div>
   );
